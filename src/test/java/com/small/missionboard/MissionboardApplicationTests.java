4a3d7298223cb3346ed4b2ca1827d620b7624dc5
alex <419725794@qq.com>
2020-04-13 01:34:24 +0800

    init

---
+++ src/test/java/com/small/missionboard/MissionboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.small.missionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
