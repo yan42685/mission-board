3e25b38463bb9a48dccdbd8570b40289ca40bb4a
alex <419725794@qq.com>
2020-04-14 00:35:39 +0800

    添加knife4j

--- src/main/java/com/small/missionboard/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.small.missionboard.config;
-
-import com.spring4all.swagger.EnableSwagger2Doc;
-import org.springframework.boot.SpringBootConfiguration;
-
-@EnableSwagger2Doc
-@SpringBootConfiguration
-public class SwaggerConfig {
-}
--- src/main/java/com/small/missionboard/controller/TestController.java
+++ src/main/java/com/small/missionboard/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.small.missionboard.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +16,12 @@ public class TestController {
     }
 
     @ApiOperation(value = "加法运算", notes = "两个整数相加", response = String.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", name = "a", dataType = "int", value = "加数", required = true, defaultValue = ""),
+            @ApiImplicitParam(paramType = "body", name = "b", dataType = "int", value = "被加数", required = true, defaultValue = "")
+    })
     @PostMapping("add")
     public String add(int a, int b) {
-        return String.valueOf(a+b);
+        return String.valueOf(a + b);
     }
 }
