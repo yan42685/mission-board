db14bd26c9024fb23a025fda852baf870046351c
alex <419725794@qq.com>
2020-04-15 16:25:34 +0800

    fix bugs

--- src/main/java/com/small/missionboard/MissionboardApplication.java
+++ src/main/java/com/small/missionboard/MissionboardApplication.java
@@ -1,6 +1,5 @@
 package com.small.missionboard;
 
-import com.small.missionboard.util.RedisUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class MissionboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MissionboardApplication.class, args);
-        System.out.println(RedisUtils.get("test1"));
     }
 
 }
