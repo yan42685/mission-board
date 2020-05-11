f4cb0a7a8390f3ad84e79d7ce599183d8ebcee47
alex <419725794@qq.com>
2020-05-11 18:00:36 +0800

    更改cors配置的方式

--- src/main/java/com/small/missionboard/config/CorsConfig.java
+++ src/main/java/com/small/missionboard/config/CorsConfig.java
@@ -1,34 +1,42 @@
 package com.small.missionboard.config;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 /**
  * WebMVC配置
  */
 @SpringBootConfiguration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
     /**
      * 开启ajax跨域请求的支持
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        //添加映射路径
-        registry.addMapping("/**")
-                //放行哪些原始域
-                .allowedOrigins("*")
-                //是否发送Cookie信息
-                .allowCredentials(true)
-                //放行哪些原始域(请求方式)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                //放行哪些原始域(头部信息)
-                .allowedHeaders("*")
-                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-                .exposedHeaders("Header1", "Header2");
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        // 创建 UrlBasedCorsConfigurationSource 配置源，类似 CorsRegistry 注册表
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 创建 CorsConfiguration 配置，相当于 CorsRegistration 注册信息
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("*")); // 允许所有请求来源
+        config.setAllowCredentials(true); // 允许发送 Cookie
+        config.addAllowedMethod("*"); // 允许所有请求 Method
+        config.setAllowedHeaders(Collections.singletonList("*")); // 允许所有请求 Header
+        // config.setExposedHeaders(Collections.singletonList("*")); // 允许所有响应 Header
+        config.setMaxAge(1800L); // 有效期 1800 秒，2 小时
+        source.registerCorsConfiguration("/**", config);
+        // 创建 FilterRegistrationBean 对象
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(source)); // 创建 CorsFilter 过滤器
+        bean.setOrder(0); // 设置 order 排序。这个顺序很重要哦，为避免麻烦请设置在最前
+        return bean;
     }
-
 }
 
 
--- src/main/java/com/small/missionboard/config/InterceptorConfig.java
+++ src/main/java/com/small/missionboard/config/InterceptorConfig.java
@@ -1,25 +1,17 @@
 package com.small.missionboard.config;
 
-import com.small.missionboard.common.LoginInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 注册拦截器
  */
-@Configuration
+@SpringBootConfiguration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //TODO 测试完毕后开启拦截器
-//        registry.addInterceptor(loginInterceptor()).addPathPatterns("/api/**");
+//        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/api/**");
     }
-
-    @Bean
-    public LoginInterceptor loginInterceptor() {
-        return new LoginInterceptor();
-    }
-
 }
